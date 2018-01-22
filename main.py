82e476de086bbd08c81e94c9ff39e156a4d9bceb
Eric Hth <eric@snapcar.com>
2018-01-23 00:41:33 +0100

    loadData() loadData_() predict() rate()

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+
+# coding: utf-8
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+def loadData():
+    test = pd.read_csv("test.csv")
+    train  = pd.read_csv("train.csv")
+    return test, train
 
-train = pd.read_csv("test.csv")
-test  = pd.read_csv("train.csv")
+def loadData_():
+    test, train = loadData()
+    train_x = train.iloc[:,:-2]
+    train_y = train['Activity']
+    test_x = test.iloc[:,:-2]
+    test_y = test['Activity']
+    return train_x, train_y, test_x, test_y
+def predict(train_x, train_y, test_x, test_y ):
+    rfc = RandomForestClassifier(n_estimators=500)
+    model = rfc.fit(train_x, train_y)
+    return rfc.predict(test_x)
+# Les deux dernières colonnes sont subject et Activity
+# Les autres colonnes sont celle où il y a des données intéressantes
 
-print(test)
-print(train)
-
-
-train_x = train.iloc[:,:-2]
-train_y = train['Activity']
-
-test_x = test.iloc[:,:-2]
-test_y = test['Activity']
+train_x, train_y, test_x, test_y = loadData_()
+pred = predict(train_x, train_y, test_x, test_y)
+print accuracy_score(test_y, pred)
