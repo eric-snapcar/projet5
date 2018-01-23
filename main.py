5007be3e2e54961f4af11012d727115f60e1c748
Eric Hth <eric@snapcar.com>
2018-01-23 01:10:41 +0100

    text_x_2 train_x_2 predict() pred model pred_2 predict( )[0]

--- main.py
+++ main.py
@@ -15,15 +15,31 @@ def loadData_():
     test_x = test.iloc[:,:-2]
     test_y = test['Activity']
     return train_x, train_y, test_x, test_y
-def predict(train_x, train_y, test_x, test_y ):
+def predict(train_x, train_y, test_x ):
     rfc = RandomForestClassifier(n_estimators=500)
     model = rfc.fit(train_x, train_y)
-    return rfc.predict(test_x)
+    return rfc.predict(test_x), model
 def rate(test_y,pred ):
     print accuracy_score(test_y, pred)
 # Les deux dernières colonnes sont subject et Activity
 # Les autres colonnes sont celle où il y a des données intéressantes
 
 train_x, train_y, test_x, test_y = loadData_()
-pred = predict(train_x, train_y, test_x, test_y)
+pred, model = predict(train_x, train_y, test_x)
+
+from sklearn.feature_selection import SelectFromModel
+select = SelectFromModel(model, prefit=True, threshold=0.005)
+train_x2 = select.transform(train_x)
+print("Train")
+print(train_x.shape)
+print(train_x2.shape)
+test_x2 = select.transform(test_x)
+print("Test")
+print(test_x.shape)
+print(test_x2.shape)
+pred_2 = predict(train_x2, train_y, test_x2)[0]
+print("Pred")
+print(pred.shape)
+print(pred_2.shape)
 rate(test_y, pred)
+rate(test_y, pred_2)
