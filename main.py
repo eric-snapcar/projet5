e40a8a54fa41b49a8f62618ca9820870875ea0cc
Eric Hth <eric@snapcar.com>
2018-01-23 01:26:22 +0100

    main.py prediction , model  predict( ) prediction_filtered predict()[0] train_x_filtered test_x_filtered filter( ) train_x text_x model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import SelectFromModel
 def loadData():
     test = pd.read_csv("test.csv")
     train  = pd.read_csv("train.csv")
@@ -15,31 +16,30 @@ def loadData_():
     test_x = test.iloc[:,:-2]
     test_y = test['Activity']
     return train_x, train_y, test_x, test_y
+
+def filter(train_x, test_x, model ):
+    select = SelectFromModel(model, prefit=True, threshold=0.005)
+    train_x_filtered = select.transform(train_x)
+    test_x_filtered = select.transform(test_x)
+    return train_x_filtered,test_x_filtered
+
+def rate(test_y,pred ):
+    print accuracy_score(test_y, pred)
+
 def predict(train_x, train_y, test_x ):
     rfc = RandomForestClassifier(n_estimators=500)
     model = rfc.fit(train_x, train_y)
     return rfc.predict(test_x), model
-def rate(test_y,pred ):
-    print accuracy_score(test_y, pred)
+
 # Les deux dernières colonnes sont subject et Activity
 # Les autres colonnes sont celle où il y a des données intéressantes
 
 train_x, train_y, test_x, test_y = loadData_()
 pred, model = predict(train_x, train_y, test_x)
-
-from sklearn.feature_selection import SelectFromModel
-select = SelectFromModel(model, prefit=True, threshold=0.005)
-train_x2 = select.transform(train_x)
-print("Train")
-print(train_x.shape)
-print(train_x2.shape)
-test_x2 = select.transform(test_x)
-print("Test")
-print(test_x.shape)
-print(test_x2.shape)
-pred_2 = predict(train_x2, train_y, test_x2)[0]
-print("Pred")
-print(pred.shape)
-print(pred_2.shape)
+print("Unfiltered accuracy")
 rate(test_y, pred)
-rate(test_y, pred_2)
+
+train_x_filtered,test_x_filtered = filter(train_x, test_x, model )
+pred_filtered = predict(train_x_filtered, train_y, test_x_filtered)[0]
+print("Filtered accuracy")
+rate(test_y, pred_filtered)
