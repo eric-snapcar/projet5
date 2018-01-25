c0c93f7a3ad3c826feaf6a87532518b0b218ea4b
Eric Hth <eric@snapcar.com>
2018-01-25 14:46:01 +0100

    .multiply( ) apply( ) axis=1 def total_price() ...

--- Main/main.py
+++ Main/main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
+data = pd.read_excel('Online Retail.xlsx')
+"""
 def total_price(x):
     return x['UnitPrice'] * x['Quantity']
-
-data = pd.read_excel('Online Retail.xlsx')
-data["Total"]=data.apply(total_price)
+data["Total"]=data.apply(total_price, axis=1) # axis = 1
+"""
+data_['Total'] = data_['UnitPrice'].multiply(data_['Quantity'])
 print data
